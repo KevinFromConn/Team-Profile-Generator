a0a5c1525de566af288eb1011b584288659fbe87
KevinFromConn <kfc21194@gmail.com>
2021-08-01 22:45:27 -0400

    fixed typos in .js files in /lib directory

--- develop/templates/internHTML.js
+++ develop/templates/internHTML.js
@@ -3,7 +3,7 @@ function internCard(data) {
       <div class="card employee-card">
       <div class="card-header">
           <h2 class="card-title">${data.name}</h2>
-          <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+          <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
       </div>
       <div class="card-body">
           <ul class="list-group">
