74e00ab6183b53050656d32a5032df9877b133f7
KevinFromConn <kfc21194@gmail.com>
2021-08-01 21:14:10 -0400

    fleshed out .js files in /lib directory

---
+++ develop/lib/Engineer.js
@@ -0,0 +1,18 @@
+const employee = require("./Employee");
+
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+
+  getGitHub() {
+    return this.github;
+  }
+
+  getRole() {
+    return "Engineer";
+  }
+}
+
+module.exports = Engineer;
